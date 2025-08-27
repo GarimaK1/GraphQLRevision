dd129ad05d6858970bc279fc9e83f62b15d7220d
Garima Khandekar <garima.k.03@gmail.com>
2025-08-27 00:37:33 -0700

    separated resolvers into its own file

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,7 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 import { typeDefs } from './schema.js';
-import db from './_db.js';
-
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => db.books,
-    book: (parent: any, args: { title: string; }) => db.books.find(book => book.title === args.title),
-    games() {
-      return db.games
-    },
-    game(parent: any, args: { id: string; }) {
-      return db.games.find((game) => game.id === args.id);
-    },
-    authors: () => db.authors,
-    author(parent: any, args: { id: string; }) {
-      return db.authors.find(author => author.id === args.id);
-    },
-    reviews() {
-      return db.reviews
-    },
-    review(parent: any, args: { id: string; }) {
-      return db.reviews.find(review => review.id === args.id);
-    }
-  },
-};
+import resolvers from './resolvers.js';
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
