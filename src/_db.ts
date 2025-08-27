5d40fd4543a3a4ded11722edc52d5596ecc26182
Garima Khandekar <garima.k.03@gmail.com>
2025-08-27 09:49:29 -0700

    added typescript types

--- src/_db.ts
+++ src/_db.ts
@@ -1,4 +1,6 @@
-const books = [
+import { Book, Game, Author, Review } from "./myTypes.js";
+
+const books: Book[] = [
   {
     title: 'The Awakening',
     author: 'Kate Chopin',
@@ -9,7 +11,7 @@ const books = [
   },
 ];
 
-const games = [
+const games: Game[] = [
   {id: '1', title: 'Zelda, Tears of the Kingdom', platform: ['Switch']},
   {id: '2', title: 'Final Fantasy 7 Remake', platform: ['PS5', 'Xbox']},
   {id: '3', title: 'Elden Ring', platform: ['PS5', 'Xbox', 'PC']},
@@ -17,13 +19,13 @@ const games = [
   {id: '5', title: 'Pokemon Scarlet', platform: ['PS5', 'Xbox', 'PC']},
 ]
 
-const authors = [
-  {id: '1', name: 'mario', verified: true},
-  {id: '2', name: 'yoshi', verified: false},
-  {id: '3', name: 'peach', verified: true},
+const authors: Author[] = [
+  {id: '1', name: 'mario', isVerified: true},
+  {id: '2', name: 'yoshi', isVerified: false},
+  {id: '3', name: 'peach', isVerified: true},
 ]
 
-const reviews = [
+const reviews: Review[] = [
   {id: '1', rating: 9, content: 'lorem ipsum', author_id: '1', game_id: '2'},
   {id: '2', rating: 10, content: 'lorem ipsum', author_id: '2', game_id: '1'},
   {id: '3', rating: 7, content: 'lorem ipsum', author_id: '3', game_id: '3'},
---
+++ src/myTypes.ts
@@ -0,0 +1,24 @@
+export type Book = {
+    title: string;
+    author: string;
+};
+
+export type Game = {
+    id: string;
+    title: string;
+    platform: string[];
+}
+
+export type Author = {
+    id: string;
+    name: string;
+    isVerified: boolean;
+}
+
+export type Review = {
+    id: string;
+    rating: number;
+    content: string;
+    author_id: string;
+    game_id: string;
+}
